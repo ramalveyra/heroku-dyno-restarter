b48ce1d395550036401c7224647e4dcae2b2b13f
creativeidz <creativeidz@gmail.com>
2014-08-27 09:00:34 +0800

    test logging out

--- web/testscript.php
+++ web/testscript.php
@@ -2,13 +2,15 @@
 
 #test log
 $msg = 'test log';
+fwrite(STDOUT,'hello logs' . PHP_EOL);
+
 if (filter_var(getenv('CUSTOM_HEROKU_LOG'), FILTER_VALIDATE_BOOLEAN)){
 	$filename = '/tmp/heroku.apache2_error.'.$_SERVER['PORT'].'.log';
 
 	$fd = fopen($filename, "a");
 	$str = "[" . date("d/m/Y H:i:s", time()) . substr((string)microtime(), 1, 8) . "] ". $msg;
 
-	if($this->debug_mode) fwrite(STDOUT,$str . PHP_EOL);// for testing 
+	fwrite(STDOUT,$str . PHP_EOL);
 	
 	fwrite($fd, $str . PHP_EOL);
 	fclose($fd); 
