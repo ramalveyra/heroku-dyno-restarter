f6fe2d1ec19351d5e8e9ed6e09d6c2eca0b524bd
creativeidz <creativeidz@gmail.com>
2014-08-27 08:29:00 +0800

    Testing logger

--- web/testscript.php
+++ web/testscript.php
@@ -1,1 +1,16 @@
-<?php print "Current time: " . time(); ?>
+<?php
+
+#test log
+$msg = 'test log';
+if (filter_var(getenv('CUSTOM_HEROKU_LOG'), FILTER_VALIDATE_BOOLEAN)){
+	$filename = '/tmp/heroku.apache2_error.'.$_SERVER['PORT'].'.log';
+
+	$fd = fopen($filename, "a");
+	$str = "[" . date("d/m/Y H:i:s", time()) . substr((string)microtime(), 1, 8) . "] ". $msg;
+
+	if($this->debug_mode) fwrite(STDOUT,$str . PHP_EOL);// for testing 
+	
+	fwrite($fd, $str . PHP_EOL);
+	fclose($fd); 
+}
+		
